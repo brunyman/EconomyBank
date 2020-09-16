05b5f8885312833c51cffc7b82a62e725a850feb
brunyman <alin_bruny@yahoo.com>
2020-09-16 10:06:07 +0300

    1.16.2 update

--- src/net/craftersland/money/Money.java
+++ src/net/craftersland/money/Money.java
@@ -174,7 +174,7 @@ public final class Money extends JavaPlugin {
 	    	is13Server = true;
 	    	is14Server = true;
 	    	return true;
-	    } else if (version.matches("1.16") || version.matches("1.16.1") | version.matches("1.16.2")) {
+	    } else if (version.matches("1.16") || version.matches("1.16.1") || version.matches("1.16.2") || version.matches("1.16.3")) {
 	    	is19Server = true;
 	    	is13Server = true;
 	    	is14Server = true;
